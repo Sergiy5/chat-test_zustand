d01375f1ea98026a601fb80d1c03d48850c12268
Serhii Kushnir <s1983kushnir@gmail.com>
2025-04-28 23:54:48 +0200

    Changed  BrowserRouter  on HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
 
+import { HashRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthContextProvider>
-          <App />
+        <App />
       </AuthContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+
+
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <AuthContextProvider>
+//         <App />
+//       </AuthContextProvider>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
