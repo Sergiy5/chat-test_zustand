c356fc4a18735923fe05a86b4a1352f4b12e6487
Serhii Kushnir <s1983kushnir@gmail.com>
2025-04-29 00:22:42 +0200

    Commit server config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	base: "/chat-test_zustand",
 	plugins: [react()],
-	server: {
-		port: 3000,
+	// server: {
+	// 	port: 3000,
 		
-	},
+	// },
 });
