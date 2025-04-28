a6b17f3a12273a9e9465bab0e7aa7666787dba01
Serhii Kushnir <s1983kushnir@gmail.com>
2025-04-29 00:17:54 +0200

    Fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: "/chat-test_zustand/",
+	base: "/chat-test_zustand",
 	plugins: [react()],
 	server: {
 		port: 3000,
