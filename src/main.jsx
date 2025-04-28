96fe9c14f32653b8c51de42f1aecb59743f96eed
Serhii Kushnir <s1983kushnir@gmail.com>
2025-04-29 00:13:43 +0200

    added / to basename

--- src/main.jsx
+++ src/main.jsx
@@ -21,7 +21,7 @@ import { AuthContextProvider } from "./context/AuthContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="chat-test_zustand">
+    <BrowserRouter basename="/chat-test_zustand">
       <AuthContextProvider>
         <App />
       </AuthContextProvider>
