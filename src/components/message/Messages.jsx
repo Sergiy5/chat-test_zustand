abbaea1d44759fb54a00b9d3102be1312b28acea
Serhii Kushnir <s1983kushnir@gmail.com>
2025-04-28 22:32:50 +0200

    Added state messages to show always existed messages

--- src/components/message/Messages.jsx
+++ src/components/message/Messages.jsx
@@ -1,31 +1,36 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useGetMessages } from "../../hooks/useGetMessages";
 import { MessageSkeleton } from "../skeletons/MessageSkeleton.jsx";
 import { Message } from "./Message";
 
 export const Messages = () => {
+  const [messages, setMessages] = useState([]);
   const { newMessages, loading } = useGetMessages();
 
   const lastMessageRef = useRef(null);
 
   useEffect(() => {
+    setMessages((prev)=>[prev, ...newMessages]);
+  }, [newMessages]);
+
+  useEffect(() => {
     setTimeout(() => {
       lastMessageRef.current?.scrollIntoView({ behavior: "smooth" });
     }, 100);
-  }, [newMessages]);
+  }, [messages]);
 
   return (
     <div className="px-4 flex-1 overflow-auto">
-      {!loading &&
-        newMessages.length > 0 &&
-        newMessages.map((message) => (
+      {/* {!loading && */}
+       { messages.length > 0 &&
+        messages.map((message) => (
           <div key={message.id} ref={lastMessageRef}>
             <Message message={message} />
           </div>
         ))}
 
-      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx} />)}
-      {!loading && newMessages.length === 0 && (
+      {loading && [...Array(1)].map((_, idx) => <MessageSkeleton key={idx} />)}
+      {!loading && messages.length === 0 && (
         <p className="text-center">Send a message to start the conversation</p>
       )}
     </div>
